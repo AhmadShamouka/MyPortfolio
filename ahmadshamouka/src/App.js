6fd27651879f36c2a114344fdfe3fd4cf118b9a9
AhmadShamouka <shamoukaahmad@gmail.com>
2024-03-08 00:30:11 +0200

    Header component and changed background color for the pages

--- ahmadshamouka/src/App.js
+++ ahmadshamouka/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import Header from "./components/header/Header";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- ahmadshamouka/src/components/header/Header.jsx
+++ ahmadshamouka/src/components/header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "./Header.css";
 const Header = () => {
-  return <div className="header-container"></div>;
+  return (
+    <div className="header-container flex center">
+      <a>HELLO</a>
+    </div>
+  );
 };
 
 export default Header;
